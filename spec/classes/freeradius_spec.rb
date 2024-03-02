68e17a4c55ca5d60b3391890e4c78a8ac086417d
Taras Korenko <taras.korenko@gmail.com>
2024-03-03 00:08:50 +0200

    Add unit tests for radiusd service

--- spec/classes/freeradius_spec.rb
+++ spec/classes/freeradius_spec.rb
@@ -2,4 +2,9 @@ require 'spec_helper'
 
 describe 'freeradius' do
   it { is_expected.to compile.with_all_deps }
+
+  it { is_expected.to contain_service('freeradius').with({
+    :ensure => 'running',
+    :enable => true,
+  }) }
 end
